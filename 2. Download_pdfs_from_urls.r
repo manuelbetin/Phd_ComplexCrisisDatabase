ac843493c5aebe41d5954ea6222090dc5e33bac9
Manuel.Betin <manuel.betin@oecd.org>
2019-12-02 10:39:08 +0100

    include merge between files and dates of requests

--- 2. Download_pdfs_from_urls.r
+++ 2. Download_pdfs_from_urls.r
@@ -27,37 +27,12 @@ load.my.packages(packages)
 #--------------------
 download_pdfs=T
 
-#---------------
-clean_IMF_urls=function(file){
-  EBS_files=url_links %>% filter(str_detect(hierarchy,"EBS"))
-  EBS_files_cor=EBS_files %>% filter(str_detect(hierarchy,"Cor")) %>% mutate(type_hierarchy="Correction")
-  EBS_files_sup=EBS_files %>% filter(str_detect(hierarchy,"Sup")) %>% mutate(type_hierarchy="Supplement")
-  EBS_files_clean=EBS_files %>% filter(!(str_detect(hierarchy,"Sup") | str_detect(hierarchy,"Cor"))) %>% mutate(type_hierarchy="Clean")
-  final_dt=rbind(EBS_files_clean,EBS_files_cor,EBS_files_sup)
-  final_dt=final_dt %>% mutate(date=as.Date(date,format="%b %d %Y"))
-  final_dt
-}
 
-find_keyword_list=function(files){
-  keywords=paste0(files$keywords,collapse=", ")
-  
-  #keywords=clean_text(keywords)
-  keywords=strsplit(keywords, ",")[[1]]
-  
-  keywords=data.frame(keywords)
-  colnames(keywords)="keywords"
-  
-  summary_keywords=keywords %>% group_by(keywords) %>% summarize(n=n()) %>% arrange(-n) %>% mutate(keywords=str_replace_all(keywords,"\\[",''),
-                                                                                  keywords=str_replace_all(keywords,"\\]",''),
-                                                                                  keywords=str_replace_all(keywords,"\\'",''),
-                                                                                  keywords=substr(keywords,2,100))
-  return(list(IMF_keywords=summary_keywords$keywords,summary_keywords=summary_keywords))
-  
-}
-
-name_links_dt="IMF_urls_clean/IMFECF_Requests_links_clean.RData"
+name_links_dt="IMF_urls_clean/Final_links_clean.RData"
 url_links=rio::import(paste0("files/",name_links_dt))
-url_links=clean_IMF_urls(url_links)
+head(url_links)
+
+url_links=url_links %>% mutate(name_file=paste0(file,"_",Loss_Date,"_",type_doc,"_RN",Review_number,"_",str_replace_all(hierarchy,"/","_")))
 
 #-------------------
 ## Download files ##
@@ -65,9 +40,10 @@ url_links=clean_IMF_urls(url_links)
 
 # download EBS alone  ------
 
-urls_clean=url_links %>% filter(type_hierarchy=="Clean")
+url_links=url_links %>% filter(type_hierarchy=="b_Clean")
+#url_links=url_links %>% filter(ID %in% c("URY","BRA","COL"))
 if(download_pdfs==T){
-  pdf_from_url(urls_clean,"files/IMFECF Program requests2")
+  pdf_from_url(url_links,"files/All programs")
 }
 
 #-------------------
@@ -84,7 +60,7 @@ files=aggregate_corpus("files/IMFECF Program requests")
 pdf_page_count(files)
 
 #find occurence of word in document
-eval_pages(files,c("Corrections"),brute_freq = T)
+#eval_pages(files,c("Corrections"),brute_freq = T)
 
 
 
