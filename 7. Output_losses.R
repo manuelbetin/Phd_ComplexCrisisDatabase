83c4ded34c7cff5c65d048de6da04f25702adc9e
Umberto Collodel <umberto.collodel@gmail.com>
2020-03-11 15:41:40 +0100

    lp with actual gdp

--- 7. Output_losses.R
+++ 7. Output_losses.R
@@ -1,7 +1,9 @@
 # Script for first results on output losses.
 # We use the local projections method described by Jorda (2005) to estimate output losses with our created indexes.
+# Download real gdp from world bank.
 
 library(lpirfs)
+library(wbstats)
 
 # Let's normalize the indexes we are interested in:
 
@@ -10,11 +12,37 @@ indexes_2normalize <- c("Currency_crisis_severe","Soft_recession","Severe_recess
 
 annual_tf_idf_norm <- annual_tf_idf %>% 
   ungroup () %>%
-  mutate_at(vars(indexes_2normalize), funs(norm = (. - mean(.,na.rm=T))/sd(.,na.rm=T)))
+  mutate_at(vars(indexes_2normalize), funs(norm = (. - mean(.,na.rm=T))/sd(.,na.rm=T))) 
+
+# Download World Bank data:
+
+growth_df <- wb(country = "countries_only",indicator = "NY.GDP.MKTP.KD.ZG",startdate = 1954, enddate = 2016) %>% 
+  rename(ISO3_Code = iso3c, 
+         year = date,
+         gdp_growth = value) %>% 
+  select(ISO3_Code, year, gdp_growth)
+
+# Use country group classification:
+
+classification <- import("../Betin_Collodel/2. Text mining IMF_data/datasets/comparison/other_data.RData") %>% 
+  select(ISO3_Code,Income_group,group) %>% 
+  filter(!duplicated(ISO3_Code))
+
+
+# Merge final df:
+
+lp_df <- merge(annual_tf_idf_norm,
+               growth_df,
+               by = c("ISO3_Code","year"),
+               all.x = TRUE) %>% 
+  merge(classification, by=c("ISO3_Code"), all.x = TRUE)
+
 
 # Local projections: 
 
-lp_df <- annual_tf_idf_norm %>% select(ISO3_Code, year, Currency_crisis_severe_norm, Soft_recession_norm, Severe_recession_norm)
+lp_df <- lp_df %>% select(ISO3_Code, year, Currency_crisis_severe_norm, Soft_recession_norm, Severe_recession_norm, gdp_growth,
+                          Income_group, group) %>% 
+  arrange(ISO3_Code, year)
 
 # Figure 3 Romer & Romer: ------
 
@@ -91,8 +119,21 @@ results_panel4 <- lp_nl_panel(annual_tf_idf_norm,
 )
 
 
-# To do: distinguish between advanced and emerging countries.
+# Figure 3 Romer & Romer (with actual gdp growth)
+
+lp_df_emerging <- lp_df %>% filter(group == "Emerging countries" | ISO3_Code == "ARG")
+lp_df_developed <- lp_df %>% filter(group == "Developped countries" & ISO3_Code != "ARG")
+
+results_panel5 <- lp_lin_panel(lp_df_emerging,
+             endog_data = "gdp_growth",
+             shock = "Currency_crisis_severe_norm",
+             panel_model = "within",
+             panel_effect = "individual",
+             confint = 1.68,
+             h = 3)
+
+plot(results_panel5)
 
 
-plot.lpirfs_lin_panel_obj
-plot_lin
+
+
