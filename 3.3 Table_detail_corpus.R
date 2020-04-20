063526e619e5f0be7ce73092daa712a206393478
Umberto Collodel <umberto.collodel@gmail.com>
2020-04-20 09:31:00 +0200

    adding more detailed description

--- 3.3 Table_detail_corpus.R
+++ 3.3 Table_detail_corpus.R
@@ -1,4 +1,8 @@
-# Description: construction table and graphs with final corpus detail
+######## Description: the script generates a histogram with the division of the 
+######## corpus into two big families, country reports and programs. A second graph
+######## shows for each family the granular ranking of different types of documents.
+######## To add: table with short explanation each type of document, whether dismissed or not, simple
+######## structure.
 
 # Remove problematic documents:
 
@@ -13,9 +17,9 @@ unique(mydata$type)
 group_data <- mydata %>% 
   mutate(family = case_when(type == "request"|type == "review"|type == "cancelation"|
                             type == "modification" ~ "Program related",
-                           type == "technical assistance" ~ "Technical assistance",
-                           type == "consultation" ~ "Consultations",
-         is.na(type) ~ "Consultations"))
+                           type == "technical assistance" ~ "Program related",
+                           type == "consultation" ~ "Country reports",
+         is.na(type) ~ "Country reports"))
 
 unique(group_data$family)
 
@@ -23,9 +27,10 @@ group_data %>%
   group_by(family) %>% 
   count() %>% 
   ungroup() %>% 
-  mutate(family = factor(family, levels = c("Technical assistance", "Program related","Consultations"))) %>%
+  mutate(family = factor(family, levels = c("Program related","Country reports"))) %>%
   ggplot(aes(family, n, fill = family)) +
   geom_col(width = .2) +
+  geom_text(aes(label=n), vjust=0.5, size=3.5)+
   coord_flip() +
   theme_bw() +
   xlab("") +
@@ -45,6 +50,7 @@ group_data %>%
   mutate(type_doc = fct_reorder(type_doc, n)) %>% 
   ggplot(aes(type_doc, n, fill = family)) +
   geom_col(width = .3) +
+  geom_text(aes(label=n), vjust= 0.5, size=3)+
   facet_wrap(~ family, ncol = 1, scales = "free_y") +
   theme_bw() +
   coord_flip() +
