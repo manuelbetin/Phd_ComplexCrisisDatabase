0a6dc2232f506ba1f7373b1fab84ec6a99464182
Umberto Collodel <umberto.collodel@gmail.com>
2020-04-21 11:08:34 +0200

    script to generate graphs probability part

---
+++ 4.8 Graphs_share_countries.R
@@ -0,0 +1,61 @@
+######## Description: the script generates graph for the probability part 
+######## Graph 1: share of countries with currency crisis, Graph 2: correlation with inflation crisis
+######## To do: automate correlation in graph 2
+
+# Average by year:
+
+mydata <- rio::import("../Betin_Collodel/2. Text mining IMF_data/datasets/tagged docs/tf_idf.RData") %>% 
+  mutate(year = as.numeric(year)) %>% 
+  group_by(ISO3_Code, year) %>%
+  summarise_if(is.double, mean, na.rm = TRUE)
+
+# Binary indicator:
+
+get_prob <- function(x){
+  ifelse(x > 0,1,0)
+}
+
+# Graph 1
+# Share of country experiencing currency crisis, 1950-2020: (5-years moving average)
+
+mydata %>% 
+  mutate_at(vars(Epidemics:World_outcomes), get_prob) %>% 
+  ungroup() %>% 
+  group_by(year) %>% 
+  filter(year >= 1946) %>% 
+  summarise(share_cc = mean(Currency_crisis_severe, na.rm = T)) %>%
+  mutate(share_cc = zoo::rollmean(share_cc, 5, align = "center", fill = NA)) %>% 
+  ggplot(aes(year, share_cc, col="share")) +
+  geom_line() +
+  theme_bw() +
+  ylab("Share of countries (%)") +
+  xlab("") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  theme(legend.position = "none")
+
+ggsave("../Betin_Collodel/2. Text mining IMF_data/output/figures/Probability/Share_CC.png")
+
+# Graph 1
+# Share of country experiencing currency crisis and inflation crisis, 1950-2020: (5-years moving average)
+# Correlation
+
+mydata %>% 
+  mutate_at(vars(Epidemics:World_outcomes), get_prob) %>% 
+  ungroup() %>% 
+  group_by(year) %>% 
+  filter(year >= 1946) %>% 
+  summarise(`Share Currency Crisis` = mean(Currency_crisis_severe, na.rm = T), `Share Inflation Crisis` = mean(Inflation_crisis, na.rm = T)) %>%
+  mutate(`Share Currency Crisis` = zoo::rollmean(`Share Currency Crisis`, 5, align = "center", fill = NA),`Share Inflation Crisis` = zoo::rollmean(`Share Inflation Crisis`, 5, align = "center", fill = NA)) %>%
+  gather("type", "share", `Share Currency Crisis`:`Share Inflation Crisis`) %>% 
+  ggplot(aes(year, share, col = type)) +
+  geom_line() +
+  theme_bw() +
+  xlab("") +
+  ylab("Share of countries (%)") +
+  annotate(geom="text", x=1965, y=0.6, label="italic(Cor): .89", parse = T) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  theme(legend.position = "bottom", legend.title = element_blank()) 
+
+ggsave("../Betin_Collodel/2. Text mining IMF_data/output/figures/Probability/Correlation_CC_Inflation_Crisis.png")
+
+  
