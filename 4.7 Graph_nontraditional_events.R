52fb59d7ef2388406364e008b8dbc52e99bb1e61
Umberto Collodel <umberto.collodel@gmail.com>
2020-04-24 11:23:42 +0200

    added vinterecept

--- 4.7 Graph_nontraditional_events.R
+++ 4.7 Graph_nontraditional_events.R
@@ -16,11 +16,13 @@ mydata %>%
   ggplot(aes(year, Social_crisis, group =1)) +
   geom_line(col = "#00BFC4") +
   geom_point(col = "#00BFC4") +
-  geom_rect(aes(xmin = 2016, xmax = 2019, ymin = -Inf, ymax = Inf),fill = "red", alpha = 0.01) +
+  geom_vline(xintercept = mydata$year[which(mydata$Social_crisis >0 & mydata$ISO3_Code == "FRA")],col = "grey10", linetype = "dotted") +
   theme_bw() +
   xlab("") +
   ylab("") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_x_continuous(breaks = c(1950,1960, 1970, 1980, 1990, 2000,2010, 2020)) + 
+  theme(axis.text=element_text(size=19), axis.title = element_text(size= 22)) +
   theme(legend.position = "none")
 
 ggsave("../Betin_Collodel/2. Text mining IMF_data/output/figures/Non_traditional_events/FRA_gilet_jaune.png")
@@ -32,11 +34,13 @@ mydata %>%
   ggplot(aes(year, Social_crisis, group =1)) +
   geom_line(col = "#00BFC4") +
   geom_point(col = "#00BFC4") +
-  geom_rect(aes(xmin = 2018, xmax = 2019, ymin = -Inf, ymax = Inf),fill = "red", alpha = 0.01) +
+  geom_vline(xintercept = mydata$year[which(mydata$Social_crisis >0 & mydata$ISO3_Code == "CHN")],col = "grey10", linetype = "dotted") +
   theme_bw() +
   xlab("") +
   ylab("") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_x_continuous(breaks = c(1950,1960, 1970, 1980, 1990, 2000,2010, 2020)) + 
+  theme(axis.text=element_text(size=19), axis.title = element_text(size= 22)) +
   theme(legend.position = "none")
 
 ggsave("../Betin_Collodel/2. Text mining IMF_data/output/figures/Non_traditional_events/CHN_hong_kong_protests.png")
@@ -52,11 +56,13 @@ mydata %>%
   ggplot(aes(year, Migration, group =1)) +
   geom_line(col = "#7CAE00") +
   geom_point(col = "#7CAE00") +
-  geom_rect(aes(xmin = 2018, xmax = 2019, ymin = -Inf, ymax = Inf),fill = "red", alpha = 0.01) +
+  geom_vline(xintercept = mydata$year[which(mydata$Migration >0 & mydata$ISO3_Code == "COL" & mydata$year > 1980)],col = "grey10", linetype = "dotted") +
   theme_bw() + 
   xlab("") +
   ylab("") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_x_continuous(breaks = c(1980, 1990, 2000,2010, 2020)) + 
+  theme(axis.text=element_text(size=19), axis.title = element_text(size= 22)) +
   theme(legend.position = "none")
 
 ggsave("../Betin_Collodel/2. Text mining IMF_data/output/figures/Non_traditional_events/COL_migration.png")
@@ -68,11 +74,13 @@ mydata %>%
   ggplot(aes(year, Migration, group =1)) +
   geom_line(col = "#7CAE00") +
   geom_point(col = "#7CAE00") +
-  geom_rect(aes(xmin = 2010, xmax = 2019, ymin = -Inf, ymax = Inf),fill = "red", alpha = 0.01) +
+  geom_vline(xintercept = mydata$year[which(mydata$Migration >0 & mydata$ISO3_Code == "LBN" & mydata$year > 1980)],col = "grey10", linetype = "dotted") +
   theme_bw() + 
   xlab("") +
   ylab("") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_x_continuous(breaks = c(1980, 1990, 2000,2010, 2020)) + 
+  theme(axis.text=element_text(size=19), axis.title = element_text(size= 22)) +
   theme(legend.position = "none")
 
 ggsave("../Betin_Collodel/2. Text mining IMF_data/output/figures/Non_traditional_events/LBN_migration.png")
