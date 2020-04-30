1045f6d651554b03b1dc7268cebe79d0533efe22
Umberto Collodel <umberto.collodel@gmail.com>
2020-04-30 17:26:23 +0200

    first steps eigencentrality for the different income groups and time buckets

--- 4.8 Analysis_centrality_crises.R
+++ 4.8 Analysis_centrality_crises.R
@@ -1,6 +1,6 @@
 ################### Description: script to analyze how each crisis moves 
 ################### within the macroeconomic system during the last 70 years i.e. centrality of the shocks.
-
+################### 
 library(igraph)
 library(gganimate)
 
@@ -11,18 +11,36 @@ library(gganimate)
 
 mydata <- rio::import("../Betin_Collodel/2. Text mining IMF_data/datasets/tagged docs/tf_idf.RData") %>% 
   mutate(year = as.numeric(year))%>% 
+  select(-Soft_recession, -Banking_crisis) %>% 
   mutate_at(vars(Epidemics:World_outcomes), funs(norm = (. - mean(.,na.rm=T))/sd(.,na.rm=T))) %>% 
   ungroup() 
 
+income_groups <- c("High income","Upper middle income","Low income")
+
+classification <- import("../Betin_Collodel/2. Text mining IMF_data/datasets/comparison/other_data.RData") %>% 
+  select(ISO3_Code,Income_group,group) %>% 
+  filter(!duplicated(ISO3_Code)) %>% 
+  mutate(Income_group = ifelse(Income_group == "Lower middle income","Low income",Income_group))
+
+mydata <- mydata %>% 
+merge(classification) 
+
+mydata_income <- income_groups %>% 
+  map(~ mydata %>% filter(Income_group == .x))
+
+
+
 # Select normalized variable and create time buckets
 
-vars_norm <- vars_select(names(mydata_norm), ends_with('norm'))
+vars_norm <- vars_select(names(mydata), ends_with('norm'))
 
 buckets <- list(
-  `1950:1975` = 1950:1975, 
-  `1975:1990` = 1975:1990,
-  `1990:2005` = 1990:2005,
-  `2005:2019` = 2005:2020)
+  `1950:1976` = 1950:1976, 
+  `1976:1992` = 1976:1992,
+  `1992:2003` = 1992:2003,
+  `2003:2013` = 2003:2013,
+  `2013:2020` = 2013:2020
+)
 
 # Creation different adjancies matrices: -----
 
@@ -80,8 +98,10 @@ corr <- buckets %>%
 network <- corr %>% 
   map(~ graph_from_adjacency_matrix(.x, mode = "undirected",diag = F, weighted = T))
 
+
+
 network %>% 
-map(plot)
+map(~ plot(.x))
 
 
 centality <- network %>% 
@@ -91,19 +111,36 @@ centality <- network %>%
   map(~ .x %>% select(category, everything())) %>% 
   map(~ .x %>% arrange(-eigencentality)) %>% 
   map(~ .x %>% mutate(category = str_remove(category,"_norm"))) %>% 
-  map(~ .x %>% mutate(category = str_replace(category,"_"," "))) %>% 
+  map(~ .x %>% mutate(category = str_replace_all(category,"_"," "))) %>% 
   map(~ .x %>% mutate(type = case_when(category == "Wars"| category == "Natural disaster" | category == "Epidemics" | category == "Migration" | category == "Social crisis" ~ "Non economic",
                                        TRUE ~ "Economic")))
 
 centality %>%
   map(~ .x %>% mutate(category = fct_reorder(category, eigencentality))) %>% 
-  map(~ .x %>% ggplot(aes(category, eigencentality, fill= type)) +
+  map(~ .x %>% ggplot(aes(category, eigencentality, fill= eigencentality)) +
         geom_col() +
         coord_flip() +
         theme_bw() +
         ylab("") +
         xlab("") +
-        labs(fill = "Type of shock:"))
+        scale_fill_gradient(low = "white",high = "red")+
+        labs(fill = "Eigencentrality"))
+
+
+# Let's try to do it similar to Manu's intensity:
+
+
+centality %>% 
+  bind_rows(.id = "period") %>% 
+  ggplot(aes(fill=period, y=eigencentality, x=category)) + 
+  geom_bar(position="stack", stat="identity", col = "black", alpha = 0.9) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(size =14,angle=90), axis.text.y = element_text(size = 14), 
+        axis.title.y = element_text(size = 14),
+        legend.title = element_blank()) +
+  scale_fill_grey() +
+  scale_color_grey() +
+  ylab("Eigencentrality")
 
 
 
@@ -111,7 +148,7 @@ centality %>%
 
 animated_df <- centality %>%
   map(~ .x %>% mutate(category = fct_reorder(category, eigencentality))) %>% 
-  bind_rows(.id = "Time span")
+  bind_rows(.id = "Time span") 
 
 plot <- animated_df %>% 
         ggplot(aes(category, eigencentality, fill= type)) +
