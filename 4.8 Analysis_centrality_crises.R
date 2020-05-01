5fd105ee6a3f4e032b508d2049975da9861ef4aa
Umberto Collodel <umberto.collodel@gmail.com>
2020-05-01 12:01:46 +0200

    same graph as intensity for eigencentrality

--- 4.8 Analysis_centrality_crises.R
+++ 4.8 Analysis_centrality_crises.R
@@ -6,14 +6,18 @@ library(gganimate)
 
 
 # Pre-process: ------
+# Creation of a nested list with income group first level and time bucket second level
 
-# Import
+# Import: 
 
 mydata <- rio::import("../Betin_Collodel/2. Text mining IMF_data/datasets/tagged docs/tf_idf.RData") %>% 
   mutate(year = as.numeric(year))%>% 
   select(-Soft_recession, -Banking_crisis) %>% 
   mutate_at(vars(Epidemics:World_outcomes), funs(norm = (. - mean(.,na.rm=T))/sd(.,na.rm=T))) %>% 
   ungroup() 
+
+# Classification data:
+
 
 income_groups <- c("High income","Upper middle income","Low income")
 
@@ -22,12 +26,15 @@ classification <- import("../Betin_Collodel/2. Text mining IMF_data/datasets/com
   filter(!duplicated(ISO3_Code)) %>% 
   mutate(Income_group = ifelse(Income_group == "Lower middle income","Low income",Income_group))
 
+# Final and different income groups df:
+
 mydata <- mydata %>% 
 merge(classification) 
 
 mydata_income <- income_groups %>% 
-  map(~ mydata %>% filter(Income_group == .x))
+  map(~ mydata %>% filter(Income_group == .x)) 
 
+names(mydata_income) <- income_groups
 
 
 # Select normalized variable and create time buckets
@@ -41,6 +48,27 @@ buckets <- list(
   `2003:2013` = 2003:2013,
   `2013:2020` = 2013:2020
 )
+
+# Nested list:
+
+mydata <- mydata %>% mutate(bucket = case_when(year >= 1950 & year <= 1976 ~ "1950:1976",
+                                         year >= 1976 & year <= 1992 ~ "1976:1992",
+                                         year >= 1992 & year <= 2003 ~ "1992:2003",
+                                         year >= 2003 & year <= 2019 ~ "2003:2019"))
+
+mydata <- split(mydata,mydata$bucket)
+                  
+
+final <- mydata_income %>% 
+  map(~ .x %>% mutate(bucket = case_when(year >= 1950 & year <= 1976 ~ "1950:1976",
+                                         year >= 1976 & year <= 1992 ~ "1976:1992",
+                                         year >= 1992 & year <= 2003 ~ "1992:2003",
+                                         year >= 2003 & year <= 2013 ~ "2003:2013",
+                                         year >= 2013 & year <= 2019 ~ "2013:2019"
+  ))) %>% 
+  map(~ split(.x, .x$bucket))
+
+
 
 # Creation different adjancies matrices: -----
 
@@ -84,63 +112,36 @@ reg[[4]] %>%
 
 # Correlation adjacency matrix:
 
-
-corr <- buckets %>% 
-  map(~ mydata %>% filter(year %in% .x)) %>% 
+corr <- mydata%>% 
   map(~ .x %>% select(vars_norm)) %>% 
   map(~ .x %>% cor(use = "complete.obs"))
+
+corr_final <- final %>%  
+  modify_depth(2, ~ .x %>% select(vars_norm)) %>% 
+  modify_depth(2, ~ .x %>% cor(use = "complete.obs"))
 
 
 
 # Calculation eigencentrality by time bucket: -----
 
 
-network <- corr %>% 
-  map(~ graph_from_adjacency_matrix(.x, mode = "undirected",diag = F, weighted = T))
+network <- corr_final %>% 
+  modify_depth(2, ~ graph_from_adjacency_matrix(.x, mode = "undirected",diag = F, weighted = T))
 
 
-
-network %>% 
-map(~ plot(.x))
-
-
-centality <- network %>% 
-  map(~ eigen_centrality(.x)$vector) %>% 
-  map(~ .x %>% stack()) %>% 
-  map(~ .x %>% rename(eigencentality = values, category = ind)) %>% 
-  map(~ .x %>% select(category, everything())) %>% 
-  map(~ .x %>% arrange(-eigencentality)) %>% 
-  map(~ .x %>% mutate(category = str_remove(category,"_norm"))) %>% 
-  map(~ .x %>% mutate(category = str_replace_all(category,"_"," "))) %>% 
-  map(~ .x %>% mutate(type = case_when(category == "Wars"| category == "Natural disaster" | category == "Epidemics" | category == "Migration" | category == "Social crisis" ~ "Non economic",
-                                       TRUE ~ "Economic")))
-
-centality %>%
-  map(~ .x %>% mutate(category = fct_reorder(category, eigencentality))) %>% 
-  map(~ .x %>% ggplot(aes(category, eigencentality, fill= eigencentality)) +
-        geom_col() +
-        coord_flip() +
-        theme_bw() +
-        ylab("") +
-        xlab("") +
-        scale_fill_gradient(low = "white",high = "red")+
-        labs(fill = "Eigencentrality"))
+centrality <- network %>% 
+  modify_depth(2, ~ eigen_centrality(.x)$vector) %>% 
+  modify_depth(2, ~ .x %>% stack()) %>% 
+  modify_depth(2, ~ .x %>% rename(eigencentrality = values, category = ind)) %>% 
+  modify_depth(2, ~ .x %>% select(category, everything())) %>% 
+  modify_depth(2, ~ .x %>% arrange(-eigencentrality)) %>% 
+  modify_depth(2,~ .x %>% mutate(category = str_remove(category,"_norm"))) %>% 
+  modify_depth(2, ~ .x %>% mutate(category = str_replace_all(category,"_"," ")))
 
 
 # Let's try to do it similar to Manu's intensity:
 
-
-centality %>% 
-  bind_rows(.id = "period") %>% 
-  ggplot(aes(fill=period, y=eigencentality, x=category)) + 
-  geom_bar(position="stack", stat="identity", col = "black", alpha = 0.9) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(size =14,angle=90), axis.text.y = element_text(size = 14), 
-        axis.title.y = element_text(size = 14),
-        legend.title = element_blank()) +
-  scale_fill_grey() +
-  scale_color_grey() +
-  ylab("Eigencentrality")
+ 
 
 
 
